a32854d0f7867cafab2d4c141a48ae6dcf8faba4
yeimiyaz <yeimi.pena@georgebrown.ca>
2024-09-28 21:49:04 -0400

    Add methods to update timer of the plugin

--- SpyderPlugin/spyder_pomodoro_timer/spyder/widgets.py
+++ SpyderPlugin/spyder_pomodoro_timer/spyder/widgets.py
@@ -21,6 +21,11 @@ from spyder.utils.icon_manager import ima
 # Third party imports
 import qtawesome as qta
 
+# --- Constants
+# ------ Time limits by default
+POMODORO_DEFAULT = 25 * 60 * 1000  # 25 mins in milliseconds
+INTERVAL = 1000
+
 # Localization
 _ = get_translation("spyder_pomodoro_timer.spyder")
 
@@ -29,9 +34,17 @@ class PomodoroTimerStatus(BaseTimerStatus):
     """Status bar widget to display the pomodoro timer"""
     ID = "pomodoro_timer_status"
     CONF_SECTION = "spyder_pomodoro_timer"
+
     def __init__(self, parent):
         super().__init__(parent)
         self.value = "25:00"
+        # Actual time limits
+        self.pomodoro_limit = POMODORO_DEFAULT
+        self.countdown = self.pomodoro_limit
+        self._interval = INTERVAL
+        self.timer.timeout.connect(self.update_timer)
+        self.timer.start(self._interval)
+
     def get_tooltip(self):
         """Override api method."""
         return "I am the Pomodoro timer!"
@@ -42,3 +55,18 @@ class PomodoroTimerStatus(BaseTimerStatus):
     def get_value(self):
         """Get current time of the timer"""
         return self.value
+    
+    def display_time(self):
+        """Calculate the time that should be displayed."""
+        minutes = int((self.countdown / (1000 * 60)) % 60)
+        seconds = int((self.countdown / 1000) % 60)
+        return f"{minutes:02d}:{seconds:02d}"
+    
+    def update_timer(self):
+        """Updates the timer and the current widget. Also, update the
+        task counter if a task is set."""
+    
+        if self.countdown > 0:
+            # Update the current timer by decreasing the current running time by one second
+            self.countdown -= INTERVAL
+            self.value = self.display_time()
